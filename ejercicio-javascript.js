8acffbbc46fe5ec7328d7913372b3a52b56b58f2
Valentina Arancibia <valentina.arancibia@genstudents.org>
2025-01-21 11:59:33 -0300

    agregar archivos necesarios

---
+++ ejercicio-javascript.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')({sigint: true})
+
+let num1 = Number(prompt("Ingrese su primer numero: "));
+let num2 = Number(prompt("Ingrese su segundo numero: "));
+let num3 = Number(prompt("Ingrese su tercer numero: "));
+
+const numberList = [num1, num2, num3];
+
